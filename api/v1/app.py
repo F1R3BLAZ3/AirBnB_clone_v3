691cd3fea981d4d7e88f63280e61d3148fbabbb5
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-30 20:00:10 +0000

    Revert "Task 1"
    This reverts commit aecc8afe8dcb0560ebaad8c0212ac43bfc265106.

--- api/v1/app.py
+++ api/v1/app.py
@@ -43,9 +43,6 @@ def not_found(error):
     return jsonify({"error": "Not found"}), 404
 
 
-def main(host='0.0.0.0', port='5000'):
-    app.run(host=host, port=port, threaded=True)
-
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST')
     port = os.getenv('HBNB_API_PORT')
@@ -53,4 +50,4 @@ if __name__ == "__main__":
         host = '0.0.0.0'
     if not port:
         port = '5000'
-    main(host, port)
+    app.run(host=host, port=port, threaded=True)
